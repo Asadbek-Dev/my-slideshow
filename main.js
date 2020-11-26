d2a873bf3f57b856bcf8f86c81e9ad576ae21a62
asadbek-dev <asadbek.dev1101@gmail.com>
2020-11-26 19:11:51 +0500

    jQueryda ham ishladim

--- main.js
+++ main.js
@@ -1,18 +1,41 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+// var slideIndex = 1;
+// showSlides(slideIndex);
 
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-  }
+// function plusSlides(n) {
+//     showSlides(slideIndex += n);
+//   }
   
-  function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var captionText = document.getElementById("caption");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slides[slideIndex-1].style.display = "block";
-  }  
+//   function showSlides(n) {
+//     var i;
+//     var slides = document.getElementsByClassName("mySlides");
+//     var captionText = document.getElementById("caption");
+//     if (n > slides.length) {slideIndex = 1}
+//     if (n < 1) {slideIndex = slides.length}
+//     for (i = 0; i < slides.length; i++) {
+//         slides[i].style.display = "none";
+//     }
+//     slides[slideIndex-1].style.display = "block";
+//   }  
+
+$(document).ready(function(){
+  var $i=0
+  $(".next").click(function(){
+    $i=$i+1
+    // alert($i)
+    $(".mySlides").eq($i-1).css("display","none")
+    $(".mySlides").eq($i).fadeOut("fast",function(){
+      $(".mySlides").eq($i+1).fadeIn("fast")
+    })
+
+  
+  }); 
+   $(".prev").click(function(){
+    $i=$i-1
+    // alert($i)
+    $(".mySlides").eq($i+1).css("display","none")
+    $(".mySlides").eq($i).fadeOut("fast",function(){
+      $(".mySlides").eq($i-1).fadeIn("fast")
+    })
+  });
+  
+});
