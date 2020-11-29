dd8c1be50652d2ded20964ac6804d98236d635ed
asadbek-dev <asadbek.dev1101@gmail.com>
2020-11-29 16:07:37 +0500

    changed js code

--- main.js
+++ main.js
@@ -18,24 +18,48 @@
 //   }  
 
 $(document).ready(function(){
-  var $i=0
-  $(".next").click(function(){
-    $i=$i+1
-    // alert($i)
-    $(".mySlides").eq($i-1).css("display","none")
-    $(".mySlides").eq($i).fadeOut("fast",function(){
-      $(".mySlides").eq($i+1).fadeIn("fast")
-    })
+  // var $i=0
+  // $(".next").click(function(){
+  //   $i=$i+1
+  //   // alert($i)
+  //   $(".mySlides").eq($i-1).css("display","none")
+  //   $(".mySlides").eq($i).fadeOut("fast",function(){
+  //     $(".mySlides").eq($i+1).fadeIn("fast")
+  //   })
 
   
-  }); 
-   $(".prev").click(function(){
-    $i=$i-1
-    // alert($i)
-    $(".mySlides").eq($i+1).css("display","none")
-    $(".mySlides").eq($i).fadeOut("fast",function(){
-      $(".mySlides").eq($i-1).fadeIn("fast")
-    })
-  });
+  // }); 
+  //  $(".prev").click(function(){
+  //   $i=$i-1
+  //   // alert($i)
+  //   $(".mySlides").eq($i+1).css("display","none")
+  //   $(".mySlides").eq($i).fadeOut("fast",function(){
+  //     $(".mySlides").eq($i-1).fadeIn("fast")
+  //   })
+  // });
+  $(".next").click(function(){
+    let foo=$(".active");
+    if(foo.next().length){
+      foo.next().addClass("active");
+      foo.removeClass("active");
+    }
+    else{
+      foo.parent().children().first().addClass("active");
+    }
+    foo.removeClass("active");
+  })
   
+
+  
+  $(".prev").click(function(){
+    let foo=$(".active");
+    if(foo.prev().length){
+      foo.prev().addClass("active");
+      foo.removeClass("active");
+    }
+    else{
+      foo.parent().children().last().addClass("active");
+    }
+    foo.removeClass("active");
+  })
 });
